ca60ed7625728831097b40e10715aeca9f7b464f
Cahyo Dwi Setiono <cahyo123dwi@gmail.com>
2024-08-15 00:09:36 +0700

    2nd commit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from ultralytics import YOLO
+import torch
+
+torch.cuda.set_device(0)
 
 # Load a model
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-model.train(data="config.yaml", epochs=3)  # train the model
+model.train(data="config.yaml", epochs=30, workers=0, batch=1)  # train the model
